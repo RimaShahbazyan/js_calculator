7a566bd5681c5e352d78c669207ff938e00793f8
rima_shahbazyan <s.rima2000rs@gmail.com>
2019-08-07 22:31:32 +0400

    LAST COMMIT

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let problem = "";
+let problem = "5*(2+0)^2-20";
 
 function printClicked ( e ) {
     problem += e;
@@ -11,28 +11,51 @@ function deleteChar () {
 }
 
 function solve () {
+    let exp = [];
+    let arr=[];
+    problem = problem.split('');
+    for (let i = 0; i < problem.length; i++) {
+        arr.push( problem[i]);
+        if(problem[i] ===')')
+        {   arr.pop();
+            let poped = arr.pop();
+            while(poped !=="(") {
+                exp.push( poped);
+                poped = arr.pop ();
+            }
+            arr.push(solveInsideBrackets(exp.reverse().join('')));
+        }
+    }
+    problem = solveInsideBrackets(arr.join('')).toString();
+    document.getElementById ("prob").innerHTML = problem;
 
-    document.getElementById ("prob").innerHTML = solveInsideBrackets(problem);
-    problem="";
 }
 
 function solveInsideBrackets ( exp ) {
+    let isNegative= false;
+    if(exp[0]==='-')
+    {
+        isNegative=true;
+        exp=exp.slice(1,exp.length)
+    }
     let nums = exp.split('*').join('_').split('/').join('_').
         split('-').join('_').split('+').join('_').
             split('^').join('_').split('%').join('_').split('_');
     let operations=exp.split(/[0.0-9]/).join("").split("");
+    if(isNegative)
+        nums[0]="-" +nums[0];
 
-    if(isNaN(Number(nums[0]))|| isNaN(Number(nums[nums.length-1])) || nums.length-1 !== operations.length)
+    if(isNaN(Number(nums[0]))|| isNaN(Number(nums[nums.length-1])) || ( operations[0]!=="" && nums.length-1 !== operations.length))
     {
        return false;
     }
     let newNums =[];
     let newOperations = [];
     let previousWasReplaced =false;
-    for (let i = operations.length -1, j=0; i > 0 ; i--) {
+    for (let i = operations.length -1, j=0; i >= 0 ; i--) {
         if (operations[i] === '^') {
             if (previousWasReplaced) {
-                newNums[j - 1] = Math.pow( Number(nums[i - 1]),newNums[j - 1]);
+                newNums[j - 1] = Math.pow( Number(nums[i]),newNums[j - 1]);
 
             }
             else {
@@ -45,18 +68,21 @@ function solveInsideBrackets ( exp ) {
         {
             newOperations.push(operations[i]);
             if(!previousWasReplaced) {
-                newNums.push (nums[i]);
+                newNums.push (nums[i+1]);
                 j++;
+            }
+            if(i===0)
+            {
+                newNums.push(nums[i]);
             }
             previousWasReplaced = false;
         }
     }
-    if(!previousWasReplaced)
-    {
-        newNums.push (nums[0]);
-    }
-    newNums.reverse();
-    newOperations.reverse();
+
+    nums = newNums.reverse();
+    newNums = [];
+    operations = newOperations.reverse();
+    newOperations = [];
     for (let i = 0, j=0; i <operations.length ; i++) {
 
         if (operations[i]==='%')
